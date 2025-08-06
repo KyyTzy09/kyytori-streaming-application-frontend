fde55251b9c7d4635fcd009b843e38ac194d22ff
Firegame224 <xg474627@gmail.com>
2025-08-06 23:09:25 +0700

    feat(libs): add checkSession function

---
+++ src/lib/session.ts
@@ -0,0 +1,17 @@
+'use server'
+
+import axios from "axios"
+import { getCookies } from "./cookies"
+import { User } from "@/common/types/user"
+
+export const checkSession = async () => {
+    const token = await getCookies()
+    if (!token) {
+        return
+    }
+    return await axios.get<{ data: User }>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user/profile`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { Roles } from "./common/enums/role";
 import { authService } from "./features/auth/services/auth.service";
 import { User } from "./common/types/user";
+import { checkSession } from "./lib/session";
 
 export default async function Middleware(req: NextRequest) {
     const pathName = req.nextUrl.pathname
-    const session = (await authService.getSession())?.data as User
+    const session = await checkSession()
 
     // Cek apakah sudah login atau belum
     if (pathName === "/signin" && session || pathName === "/signup" && session) {
         return NextResponse.redirect(new URL("/dashboard", req.url))
     }
 
-    if (pathName.startsWith("/dashboard/admin") && session.role !== Roles.Admin) {
+    if (pathName.startsWith("/dashboard/admin") && session?.data.data.role !== Roles.Admin) {
         return NextResponse.redirect(new URL("/signin", req.url))
     }
 
