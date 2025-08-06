d98604a2999ad666d5e133e2a8e5f49e8dadaead
Firegame224 <xg474627@gmail.com>
2025-08-06 23:44:16 +0700

    fix(helpers): remove validate status for better safety response

--- src/common/helpers/axios.ts
+++ src/common/helpers/axios.ts
@@ -1,10 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    validateStatus: (status) => {
-        return status <= 401 && status !== 400;
-    }
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
 })
 
 export const apiClient = async <T>(config: AxiosRequestConfig): Promise<T> => {
