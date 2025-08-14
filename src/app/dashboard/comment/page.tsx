f4b591490d0471ca05c6f77af5cea9ff03f88d3a
Firegame224 <xg474627@gmail.com>
2025-08-14 22:24:27 +0700

    refactor(favorite-page, comment-page): show not found image when no favorites or comments

--- src/app/dashboard/comment/page.tsx
+++ src/app/dashboard/comment/page.tsx
@@ -4,15 +4,31 @@ import CommentHeader from "@/features/comment/components/comment-header";
 import UserCommentCard from "@/features/comment/components/cards/comment-user-card";
 import { useGetUserComment } from "@/features/comment/hooks/comment-hook";
 import React from "react";
+import Image from "next/image";
 
 export default function CommentPage() {
   const { data: comment, isPending } = useGetUserComment();
   return (
     <div className="w-full flex flex-col min-h-screen p-5">
       <CommentHeader komentarLength={comment?.data?.length || 0} />
-      <div className="w-full flex flex-col min-h-screen">
-        <UserCommentCard data={comment?.data!} isLoading={isPending} />
-      </div>
+      {!isPending && comment?.data?.length! > 0 ? (
+        <div className="w-full flex flex-col min-h-screen">
+          <UserCommentCard data={comment?.data!} isLoading={isPending} />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full min-h-screen">
+          <Image
+            src={"/img/not-found-404.png"}
+            alt="404"
+            width={300}
+            height={300}
+            className="w-56 h-56 md:w-80 md:h-80"
+          />
+          <p className="text-white font-semibold text-lg">
+            Anda belum menambahkan komentar apapun
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/dashboard/favorite/page.tsx
+++ src/app/dashboard/favorite/page.tsx
@@ -3,16 +3,30 @@
 import AnimeFavoriteCard from "@/features/favorite/components/cards/favorite-anime-card";
 import FavoriteHeader from "@/features/favorite/components/favorite-header";
 import { useGetUserFavorites } from "@/features/favorite/hooks/favorite-hook";
+import Image from "next/image";
 import React from "react";
 
 export default function FavoritePage() {
   const { data: favorite, isPending } = useGetUserFavorites();
   return (
     <div className="w-full min-h-screen p-5">
-      <FavoriteHeader favoriteLength={favorite?.data.length!} />
-      <div className="w-full h-full bg-gray-50 shadow shadow-black rounded-md p-5">
-        <AnimeFavoriteCard data={favorite?.data!} isLoading={isPending} />
-      </div>
+      <FavoriteHeader favoriteLength={favorite?.data.length! || 0} />
+      {!isPending && favorite?.data?.length! > 0 ? (
+        <div className="w-full h-full bg-gray-50 shadow shadow-black rounded-md p-5">
+          <AnimeFavoriteCard data={favorite?.data!} isLoading={isPending} />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full min-h-screen">
+          <Image
+            src={"/img/not-found-404.png"}
+            alt="404"
+            width={300}
+            height={300}
+            className="w-56 h-56 md:w-80 md:h-80"
+          />
+          <p className="text-white font-semibold text-lg">Anda belum menambahan anime apapun</p>
+        </div>
+      )}
     </div>
   );
 }
