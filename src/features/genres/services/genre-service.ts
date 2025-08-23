27c5fa3eba23deaac01c5184dc0e85b0572ff119
Firegame224 <xg474627@gmail.com>
2025-08-23 15:56:42 +0700

    fix(genre-service): change api url in apiClient

--- src/features/genres/services/genre-service.ts
+++ src/features/genres/services/genre-service.ts
@@ -4,10 +4,10 @@ import { pagination } from "@/features/anime/services/anime-service"
 
 export const genreService = {
     async getAllGenres() {
-        return await apiClient<{ data: Genres[] }>({ url: `/anime/genres` })
+        return await apiClient<{ data: Genres[] }>({ url: `/genres/get` })
     },
 
     async getAnimeByGenre(data: { genre: string, page?: number }) {
-        return await apiClient<{ data: Anime[], pagination: pagination }>({ url: `/anime/anime-genre?genre=${data.genre}&page=${data.page}` })
+        return await apiClient<{ data: Anime[], pagination: pagination }>({ url: `/genres/anime-genre?genre=${data.genre}&page=${data.page}` })
     }
 }
